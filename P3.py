d6724c24b1dfb07faf6b680502b2d7b3dbc59bf0
Shreyas Bodani <sbodani29@gmail.com>
2022-04-12 10:24:04 +0530

    More new programs

---
+++ P3.py
@@ -0,0 +1,14 @@
+# Let's find the area of a rectangle!
+""" To write a multiline
+comment we use : " " " :"""
+x = 5.34
+y = 10
+z = y - x + 8
+
+
+Area_of_rectangle = x*y
+print(Area_of_rectangle)
+print(type(x))
+
+print(type(y))
+print(type(z))
